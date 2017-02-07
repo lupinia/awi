abc9d07f660aa5f8b6eea8835dd29a6f0ef0dee9
Natasha L <webmaster@lupinia.net>
2017-02-06 20:14:57 -0500

    secondlife:  Code cleanup to improve consistency and readability, added verbose_name and help_text attributes where necessary.

--- secondlife/models.py
+++ secondlife/models.py
@@ -7,13 +7,18 @@
 #	=================
 
 from django.db import models
+from django.utils.safestring import mark_safe
 
 class security_control(models.Model):
-	GRID_OPTIONS=(('sl','Second Life'),('opensim_lup','OpenSim (Lupinia)'))
+	GRID_OPTIONS = (('sl','Second Life'),('opensim_lup','OpenSim (Lupinia)'))
 	
-	name=models.CharField(max_length=150)
-	key=models.CharField(max_length=255,unique=True)
-	auth=models.BooleanField()
-	grid=models.CharField(max_length=20,choices=GRID_OPTIONS,default='sl')
+	name = models.CharField(max_length=150, help_text=mark_safe('The <a href="http://wiki.secondlife.com/wiki/Category:LSL_Avatar/Name" target="_BLANK">legacy name</a> of this user.'))
+	key = models.CharField(max_length=255, unique=True, help_text=mark_safe('The unique identifier for this user.  <a href="http://wiki.secondlife.com/wiki/Category:LSL_Key" target="_BLANK">More info</a>.'))
+	auth = models.BooleanField(verbose_name='authorized?', help_text='Check this box to allow this user access to in-world secure systems/areas.')
+	grid = models.CharField(max_length=20, choices=GRID_OPTIONS, default='sl', help_text='Select the virtual world/"grid" for this user.')
+	
 	def __unicode__(self):
 		return self.name
+	
+	class Meta:
+		verbose_name = 'authorized user'
