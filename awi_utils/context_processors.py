4a977ba7ffefd3933cc8652ffed54741c2e7f296
Natasha L <webmaster@lupinia.net>
2017-11-28 13:56:38 -0500

    CookieLaw:  Styling improvements to banner, and banner now only displays on .eu domains.

--- awi_utils/context_processors.py
+++ awi_utils/context_processors.py
@@ -10,9 +10,15 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 
 def site(request):
+	if '.eu' in request.get_host():
+		show_cookie_banner = True
+	else:
+		show_cookie_banner = False
+	
 	return {
 		'site':Site.objects.get_current(), 
 		'domain_name':request.get_host(), 
+		'cookie_banner':show_cookie_banner,
 	}
 
 def settings_vars(request):
