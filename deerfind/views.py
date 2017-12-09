68f56d5214133eea2c4fc800cbd75c02bae97318
Natasha L <webmaster@lupinia.net>
2017-12-09 17:58:28 -0500

    DeerFind:  404 page now performs a search automatically when a match cannot be found in pointer objects or finder functions.  It also always links to the search page to search for URL terms.  This release also includes code cleanup in DeerFind.views.

--- deerfind/views.py
+++ deerfind/views.py
@@ -4,9 +4,14 @@
 #	
 #	=================
 #	Views
-#	not_found:	Tries to find a redirect before giving up and showing a 404 error.  Uses a list of standard finder functions (settings.DEERFIND_FINDERS), one per content type.
-#	g2_finder:	Finder for Gallery2 legacy support.  It's in this file because that's handled by this app.
+#	not_found:  Tries to find a redirect before giving up and showing a 404 error.  Uses a list of standard finder functions (settings.DEERFIND_FINDERS), usually one per app or content type (but not necessarily).
+#	search_view:  Primary search results/search form view.
+#	
+#	Helper Functions  
+#	g2_finder:  Finder for Gallery2 legacy support.  It's in this file because that's handled by this app.
 #	=================
+
+import os
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -16,44 +21,14 @@ from django.template import Context, loader
 from django.utils.module_loading import import_string
 
 from haystack.generic_views import FacetedSearchView
+from haystack.query import SearchQuerySet, SQ
+from haystack.inputs import AutoQuery
 
 from awi_access.models import check_mature, access_search
 from deerfind.forms import simple_search_form
 from deerfind.models import pointer, hitlog
 
-#	404 Handling
-def g2_finder(request):
-	import os
-	from deerfind.models import g2map
-	return_data = (False,'')
-	
-	basename=os.path.basename(request.path)
-	if '.g2' in basename:
-		search_slug_list = basename.split('.')
-		search_slug = search_slug_list[0]
-	else:
-		search_slug = basename
-	
-	if '.g2' in basename or request.GET.get('g2_itemId',''):
-		if '.g2' in basename:
-			gallery_id = search_slug
-		else:
-			gallery_id = request.GET.get('g2_itemId','')
-		
-		if gallery_id and gallery_id.isdigit():
-			gallery_check=g2map.objects.select_related('category', 'image').filter(g2id=gallery_id)
-			if gallery_check.exists():
-				if gallery_check[0].category:
-					access_check = gallery_check[0].category.can_view(request)
-					if access_check[0]:
-						return_data = (True,gallery_check[0].category.get_absolute_url())
-				elif gallery_check[0].image:
-					access_check = gallery_check[0].image.can_view(request)
-					if access_check[0]:
-						return_data = (True,gallery_check[0].image.get_absolute_url())
-	
-	return return_data
-
+#	404 Handler
 def not_found(request):
 	return_url = False
 	
@@ -111,7 +86,7 @@ def not_found(request):
 		return HttpResponsePermanentRedirect(return_url)
 	else:
 		#	If all else fails, display the 404 page
-		template=loader.get_template('deerfind/404.html')
+		template = loader.get_template('deerfind/404.html')
 		
 		#	Get the attempted URL to display to the user
 		if request.META.get('QUERY_STRING',False):
@@ -120,10 +95,69 @@ def not_found(request):
 			context_path=request.path
 		context=Context({'old_url':context_path})
 		
+		#	Run a search query, in case what they're looking for can be found with search.
+		#	First, let's clean up the URL and turn it into something we can search.
+		basename = os.path.basename(request.path)
+		if '.' in basename:
+			search_path = basename.split('.')[0]
+		else:
+			search_path = basename
+		
+		sqs = access_search(SearchQuerySet(), request).filter(SQ(url=AutoQuery(search_path)) | SQ(title=AutoQuery(search_path)))
+		if sqs.count():
+			result = sqs.best_match()
+			context['search_result'] = True
+			
+			if settings.SEARCH_RESULT_DATA.get(result.content_type(), {}).get('display_name', False):
+				context['search_result_title'] = '%s (%s)' % (result.title, settings.SEARCH_RESULT_DATA[result.content_type()]['display_name'])
+			else:
+				context['search_result_title'] = result.title
+			
+			context['search_result_url'] = result.url
+		
+		else:
+			context['search_result'] = False
+		
+		context['search_query'] = search_path.replace(' ', '+')
+		
 		#	Here's the 404 template.  Sorry we couldn't find what you were looking for!
 		return HttpResponseNotFound(content=template.render(context), content_type='text/html; charset=utf-8')
 
-#	Search Views
+
+#	Finder:  Gallery2 Legacy Compatibility
+def g2_finder(request):
+	from deerfind.models import g2map
+	return_data = (False,'')
+	
+	basename = os.path.basename(request.path)
+	if '.g2' in basename:
+		search_slug_list = basename.split('.')
+		search_slug = search_slug_list[0]
+	else:
+		search_slug = basename
+	
+	if '.g2' in basename or request.GET.get('g2_itemId',''):
+		if '.g2' in basename:
+			gallery_id = search_slug
+		else:
+			gallery_id = request.GET.get('g2_itemId','')
+		
+		if gallery_id and gallery_id.isdigit():
+			gallery_check=g2map.objects.select_related('category', 'image').filter(g2id=gallery_id)
+			if gallery_check.exists():
+				if gallery_check[0].category:
+					access_check = gallery_check[0].category.can_view(request)
+					if access_check[0]:
+						return_data = (True,gallery_check[0].category.get_absolute_url())
+				elif gallery_check[0].image:
+					access_check = gallery_check[0].image.can_view(request)
+					if access_check[0]:
+						return_data = (True,gallery_check[0].image.get_absolute_url())
+	
+	return return_data
+
+
+#	Primary Search View
 class search_view(FacetedSearchView):
 	form_class = simple_search_form
 	facet_fields = ['pub_date', 'category', 'tags']
