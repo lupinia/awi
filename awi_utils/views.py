0dff086b44eb5c00c5bf44ef91f34cf997d1d4ab
Natasha L <webmaster@lupinia.net>
2018-03-05 16:45:43 -0500

    Utils:  Added a generic placeholder template view for occasions when a basic template needs to be displayed instead of another feature.

--- awi_utils/views.py
+++ awi_utils/views.py
@@ -9,6 +9,12 @@
 import simplejson
 
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def json_response(request, data=''):
 	return HttpResponse(simplejson.dumps(data), content_type='application/json')
+
+class placeholder(TemplateView):
+	def get_context_data(self, **kwargs):
+		context=super(placeholder,self).get_context_data(**kwargs)
+		return context
