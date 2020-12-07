c8b267d81651e24df5b2265a89ea73e9fc2f7298
Natasha L <webmaster@lupinia.net>
2020-12-06 23:11:49 -0500

    DeerLand:  Added description/notes fields and __unicode__ methods to `estate` and `region` models, added timestamp fields to `estate`, and added is_active toggle to `region` to indicate regions that are no longer present (useful for tracking older data)

--- deerland/models.py
+++ deerland/models.py
@@ -15,6 +15,12 @@ class estate(models.Model):
 	slug = models.SlugField(unique=True)
 	grid = models.CharField(max_length=100, choices=settings.GRID_OPTIONS, help_text='Select the virtual world/"grid" for this estate and its regions.')
 	owner = models.ForeignKey('usertools.person', on_delete=models.SET_NULL, null=True, blank=True)
+	description = models.TextField(blank=True, null=True)
+	timestamp_mod = models.DateTimeField(auto_now=True, db_index=True, verbose_name='date/time modified')
+	timestamp_post = models.DateTimeField(default=timezone.now, db_index=True, verbose_name='date/time created')
+	
+	def __unicode__(self):
+		return '%s (%s)' % (self.name, self.get_grid_display())
 
 class region(models.Model):
 	RATING_OPTIONS = (
@@ -26,11 +32,16 @@ class region(models.Model):
 	
 	name = models.CharField(max_length=64)
 	estate = models.ForeignKey(estate, on_delete=models.PROTECT)
+	notes = models.TextField(blank=True, null=True)
 	
 	rating = models.CharField(max_length=10, choices=RATING_OPTIONS, default='UNKNOWN')
 	data_pos_x = models.IntegerField(null=True, blank=True)
 	data_pos_y = models.IntegerField(null=True, blank=True)
+	is_active = models.BooleanField(blank=True, default=True)
 	
 	timestamp_mod = models.DateTimeField(auto_now=True, db_index=True, verbose_name='date/time modified')
 	timestamp_post = models.DateTimeField(default=timezone.now, db_index=True, verbose_name='date/time created')
 	timestamp_check = models.DateTimeField(blank=True, null=True, verbose_name='date/time of last data check')
+	
+	def __unicode__(self):
+		return self.name
