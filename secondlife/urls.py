4b9cc1c80b1a88c4cf68a81df44709bbd8609f39
Natasha L <webmaster@lupinia.net>
2020-12-31 05:11:28 -0500

    secondlife:  Update urls.py to remove deprecated `patterns` function from URL definitions. (#45)
    Something I probably should've done a long time ago, since it was already deprecated when I updated to Django 1.8.x, but I was hoping to remove this entire module before it became a problem.  I still look forward to removing this, but I don't quite have its replacement (DeerGuard SL) ready, so I still need to keep it limping along for now.

--- secondlife/urls.py
+++ secondlife/urls.py
@@ -6,12 +6,12 @@
 #	URL Map
 #	=================
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from secondlife import views
 
-urlpatterns = patterns('',
+urlpatterns = [
 	url(r'^security_sl',views.security_response, {'system':'lupinia-old', 'zone':'LMC'} ,name='sl_security'),
 	url(r'^security_oslup',views.security_response, {'system':'lupinia-os-old', 'zone':'LMC'} ,name='oslup_security'),
 	url(r'^security_check.php',views.security_response, {'system':'lupinia-old', 'zone':'LMC'} ,name='legacy_sl_security'),
 	url(r'^opensim_security_check.php',views.security_response, {'system':'lupinia-os-old', 'zone':'LMC'} ,name='legacy_oslup_security'),
-)
+]
