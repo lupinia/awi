dfb3ea0d71d190510584b8c554c418abfd8b3864
Natasha L <webmaster@lupinia.net>
2021-05-05 19:56:05 -0400

    DeerCoins:  Custom admin view for coin objects

--- deercoins/admin.py
+++ deercoins/admin.py
@@ -9,7 +9,17 @@
 from django.contrib import admin
 from deercoins.models import *
 
-admin.site.register(coin)
+class coins_admin(admin.ModelAdmin):
+	list_select_related = True
+	list_display = ('code', 'country', 'year', 'currency', 'value', 'is_note', 'is_special_issue', 'acquired_date', 'timestamp_mod', 'timestamp_post')
+	list_filter = ['is_note', 'is_special_issue', 'country', 'currency', 'acquired_date', 'timestamp_post', 'timestamp_mod']
+	search_fields = ['code', 'coin_notes', 'condition_notes', 'acquired_notes']
+	
+	class Media:
+		js=['js/foldable-list-filter.js',]
+		css={'all':['css/foldable-list-filter.css',],}
+
+admin.site.register(coin, coins_admin)
 admin.site.register(code_alias)
 admin.site.register(currency)
 admin.site.register(country)
