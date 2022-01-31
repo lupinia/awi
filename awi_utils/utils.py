d52ce07587044c67bd9a08ba6d7ea77925ec0161
Natasha L <webmaster@lupinia.net>
2022-01-31 13:15:17 -0500

    Awi Utils: Added function to build a valid model field choices iterable from a dictionary

--- awi_utils/utils.py
+++ awi_utils/utils.py
@@ -127,3 +127,15 @@ def hash_sha256(source):
 def hash_sha512(source):
 	hash = hashlib.sha512(source)
 	return hash.hexdigest()
+
+
+#	Since Django is stupidly picky about what it will accept for model field choices, 
+#	I have to write a function that will turn the output of .keys() from a dict into tuple pairs.
+def dict_key_choices(source_dict):
+	keys = source_dict.keys()
+	tuple_list = []
+	
+	for key in keys:
+		tuple_list.append((key, key))
+	
+	return tuple_list
