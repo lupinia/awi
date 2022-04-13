ecb30c277ba032abe184e5b2175efbc65050bcb5
Natasha L <webmaster@lupinia.net>
2022-04-12 20:39:27 -0400

    Awi Utils: Extensive improvements to random sequence functionality: - Added `exclude_zero` parameter to `rand_int_list()` to completely omit zeros from the selectable range. - Code cleanup to use more standardized naming. - Added new `rand_char_list()` function; similar to `rand_int_list()`, but returns a list of ASCII strings instead, with or without numbers.

--- awi_utils/utils.py
+++ awi_utils/utils.py
@@ -9,6 +9,7 @@
 import hashlib
 import platform
 import random
+import string
 
 from django.conf import settings
 from django.contrib.sites.models import Site
@@ -137,22 +138,25 @@ def hash_sha512(source):
 #	Generate a random integer sequence of a specific length.
 #		duplicates:  Boolean.  If True, sequential duplicates are allowed.
 #		first_zero:  Boolean.  If True, first digit in sequence can be a zero.
-def rand_int_list(length=1, duplicates=False, first_zero=False):
+#		exclude_zero:  Boolean.  If True, no zeros will be part of the sequence.
+def rand_int_list(length=1, duplicates=False, first_zero=False, exclude_zero=False):
 	# Handle bad inputs, just in case
 	if length < 1:
 		return []
 	elif length > 1024:
 		length = 1024
 	
-	integer_generator = random.SystemRandom()
+	generator = random.SystemRandom()
 	int_list = []
 	i = 0
 	while i < length:
-		if not first_zero and not i:
+		if exclude_zero:
+			range_start = 1
+		elif not first_zero and not i:
 			range_start = 1
 		else:
 			range_start = 0
-		int_list.append(integer_generator.randrange(range_start, 10))
+		int_list.append(generator.randrange(range_start, 10))
 		i += 1
 		
 		# Check for sequential duplicates
@@ -163,6 +167,51 @@ def rand_int_list(length=1, duplicates=False, first_zero=False):
 	
 	return int_list
 
+#	Generate a random letter sequence of a specific length.
+#		duplicates:  Boolean.  If True, sequential duplicates are allowed.
+#		mix_case:  Boolean.  If True, both upper and lowercase letters will be included.  Else, only uppercase will be returned.
+#		exclude:  List.  Any characters in this list will be removed from the possible selections, case-insensitively.
+#		include_digits:  Boolean.  If True, numbers will also be added to the selection pool.
+#		first_zero:  Boolean.  If True, first digit in sequence can be a zero.  Only has an effect if include_digits is also true.
+def rand_char_list(length=1, duplicates=False, mix_case=False, exclude=[], include_digits=False, first_zero=False):
+	# Handle bad inputs, just in case
+	if length < 1:
+		return []
+	elif length > 1024:
+		length = 1024
+	
+	# Set up the initial range
+	if mix_case:
+		char_range = string.ascii_letters
+	else:
+		char_range = string.ascii_uppercase
+	
+	if include_digits:
+		char_range += '0123456789'
+	
+	for ex in exclude:
+		char_range = char_range.replace(ex.lower(), '')
+		char_range = char_range.replace(ex.upper(), '')
+	
+	generator = random.SystemRandom()
+	char_list = []
+	i = 0
+	
+	while i < length:
+		char_list.append(generator.choice(char_range))
+		i += 1
+		
+		# Check for sequential duplicates
+		if duplicates == False and i > 1:
+			if char_list[i-1].lower() == char_list[i-2].lower():
+				char_list.pop()
+				i -= 1
+		elif not first_zero and i == 1:
+			if char_list[0] == '0':
+				char_list.pop()
+				i -= 1
+	
+	return char_list
 
 #	Since Django is stupidly picky about what it will accept for model field choices, 
 #	I have to write a function that will turn the output of .keys() from a dict into tuple pairs.
