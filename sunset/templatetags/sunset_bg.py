e9806980239d749a9cf1c1c7c12207d55cc15da3
Natasha L <webmaster@lupinia.net>
2023-05-19 00:47:50 -0400

    Sunset: Major alterations to separate background selection logic from data output (implementing #65): - Main template tag is now called `bg_select`, and accepts keywords or context, but does not directly return anything. - Secondary template tag, `bg_data`, returns a dictionary of background image data (title, url, etc).  See issue for further details. - Background image data is now handled as a single dictionary, to improve efficiency, and lay the groundwork for #61. - Original `bg_filename` and `bg_info` template tags have been deprecated. - Moved output of `bg_info` tag to template code in `base_core.html`. - Moved body tag background image attribute and other class-setting out of `sunset/bg_image.html` and into `base_blank.html`. - Removed `sunset/bg_image.html` and all include code for it. - Updated all templates with manually-specified background tags to use the new block and syntax. - Major code cleanup in `sunset_bg.py` to remove unnecessary imports, reduce global variables to a minimum, and store/retrieve/handle data more efficiently. - Addressed minor content filtering issues by adding `access_query` filtering to all querysets.  In practice, this never posed an actual security issue, but omitting this was a mistake nonetheless. - Extended cache times for background images in most views.

--- sunset/templatetags/sunset_bg.py
+++ sunset/templatetags/sunset_bg.py
@@ -8,102 +8,88 @@
 #	bg_info:		Returns text giving a title and info link for the current background
 #	=================
 
+import json
 import random
 
 from django import template
 from django.conf import settings
 from django.core.cache import cache
-from django.db.models import Q
-from django.utils.safestring import mark_safe
 
+from awi_access.models import access_query
 from awi_utils.utils import notify
-from sunset.models import image, background_tag
+from sunset.models import image
 
 register = template.Library()
-display_footer_info = True
-cur_background_title = ""
-cur_background_info_url = ""
+blank_bg = {
+	'url':'%ssunset/blank.png' % settings.STATIC_URL,
+	'display_footer_info':False,
+}
+global bg_data
 
 def check_cache(prefix):
 	check = cache.get('sunsetbg_%s' % prefix)
 	if check is None:
 		return (False, {})
 	else:
-		values = {}
-		values['url'], values['title'], values['info_url'] = check.split('|!|')
+		values = json.loads(check)
 		return (True, values)
 
-def store_cache(prefix, url, info_title="", info_url="", timeout=300):
-	value = '%s|!|%s|!|%s' % (url, info_title, info_url)
-	cache.set('sunsetbg_%s' % prefix, value, 300)
+def store_cache(prefix, data, timeout=300):
+	value = json.dumps(data)
+	cache.set('sunsetbg_%s' % prefix, value, timeout)
 
-def set_bg(image_obj, cache_key="", cache_timeout=0):
-	global cur_background_title
-	global cur_background_info_url
-	global display_footer_info
-	
+def set_bg(image_obj, display_footer_info, cache_key="", cache_timeout=0):
+	bg_data = {}
 	cur_bg_asset = image_obj.assets.get(type='bg')
 	if cur_bg_asset:
-		bg_url = cur_bg_asset.get_url()
+		bg_data['url'] = cur_bg_asset.get_url()
+		bg_data['display_footer_info'] = display_footer_info
 		if display_footer_info:
-			cur_background_title = str(image_obj)
-			cur_background_info_url = image_obj.get_absolute_url()
+			bg_data['title'] = str(image_obj)
+			bg_data['info_url'] = image_obj.get_absolute_url()
 		
 		if cache_key and cache_timeout:
-			store_cache(cache_key, bg_url, cur_background_title, cur_background_info_url, cache_timeout)
-		
-		return bg_url
-	else:
-		return ''
-
-def set_bg_from_cache(cache_data):
-	global cur_background_title
-	global cur_background_info_url
-	global display_footer_info
+			store_cache(cache_key, bg_data, cache_timeout)
 	
-	if display_footer_info:
-		cur_background_title = cache_data.get('title', '')
-		cur_background_info_url = cache_data.get('info_url', '')
-	
-	return cache_data.get('url', '')
+	return bg_data
 
 
 @register.simple_tag(takes_context=True)
-def bg_filename(context, input_string=''):
-	global display_footer_info
+def bg_select(context, input_string=''):
+	global bg_data
+	display_footer_info = False
 	cache_key = ""
-	cache_timeout = 300
+	cache_timeout = 900
 	
-	if context.get('image', False):
+	if context.get('image', False) and context.get('image', False).can_view(context.get('request', False))[0]:
 		# First check:  If bg_type is current_image, just set the current image as the background.
 		bg_selected = context.get('image', False)
 		display_footer_info = False
-		return set_bg(bg_selected)
+		bg_data = set_bg(bg_selected, display_footer_info)
+		return ''
 	else:
 		display_footer_info = True
-		bg_query = image.objects.filter(assets__type='bg').select_related('cat')
-		has_cat = False
+		bg_query = image.objects.filter(assets__type='bg').filter(access_query(context.get('request', False))).select_related('cat')
 		
 		# Step 1:  Define query parameters, as narrowly as possible
 		if context.get('category', False):
 			# Second check:  Try to pick something from the current category, or a specified one.
 			cur_cat_id = context['category'].pk
 			cache_key = 'cat_%d' % cur_cat_id
-			cache_timeout = 900
-			has_cat = cur_cat_id
+			cache_timeout = 1800
 			
 			bg_query_cat = bg_query.filter(cat_id=cur_cat_id).filter(featured=True)
 			# Special case:  If there's nothing directly in this category, use the category's background tag
 			if bg_query_cat:
 				bg_query = bg_query_cat
 			else:
-				bg_query = bg_query.filter(bg_tags__category__id=has_cat)
+				bg_query = bg_query.filter(bg_tags__category__id=cur_cat_id)
 		
 		elif context.get('tag', False):
 			# Third check:  If bg_type is current_tag, try to pick something from the current tag.
 			cur_tag = context.get('tag', False)
 			cache_key = 'tag_%d' % cur_tag.pk
-			cache_timeout = 900
+			cache_timeout = 600
 			bg_query = bg_query.filter(tags=cur_tag).filter(featured=True)
 		
 		elif input_string:
@@ -121,31 +107,28 @@ def bg_filename(context, input_string=''):
 		cache_status, cached_data = check_cache(cache_key)
 		if cache_status:
 			# Yay!  This was easy
-			return set_bg_from_cache(cached_data)
+			bg_data = cached_data
+			return ''
 		
 		else:
 			if not bg_query:
 				# If nothing is set yet, use a default image
-				bg_query = image.objects.filter(bg_tags__default=True, assets__type='bg').select_related('cat')
+				bg_query = image.objects.filter(bg_tags__default=True, assets__type='bg').filter(access_query(context.get('request', False))).select_related('cat')
 			
 			# Ok, time to check for results and actually render something!
 			if bg_query:
-				return set_bg(random.choice(bg_query), cache_key, cache_timeout)
+				bg_data = set_bg(random.choice(bg_query), display_footer_info, cache_key, cache_timeout)
+				return ''
 			
 			else:
 				# Despite our best efforts, there is no suitable background image.  So we'll use a static default.
 				display_footer_info = False
 				if settings.SUNSET_BG_NOTIFY_FAIL:
 					notify(subject='Sunset Background Failure', msg="A background image object could not be found.")
-				return '%ssunset/blank.png' % settings.STATIC_URL
+				bg_data = blank_bg
+				return ''
 
-@register.simple_tag
-def bg_info(input_string=''):
-	global cur_background_title
-	global cur_background_info_url
-	global display_footer_info
-	
-	if display_footer_info:
-		return mark_safe('Background Photo:  <a href="%s">%s</a>' % (cur_background_info_url, cur_background_title))
-	else:
-		return ' ';
+@register.simple_tag()
+def get_bg_data():
+	global bg_data
+	return bg_data
