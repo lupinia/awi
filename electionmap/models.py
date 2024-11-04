3aa1e7f914683a3d720e6d66b173012ef5e07458
Natasha L <webmaster@lupinia.net>
2024-11-03 19:53:14 -0500

    ElectionMap: Added model fields and default calculations for election day and poll closing time

--- electionmap/models.py
+++ electionmap/models.py
@@ -6,6 +6,7 @@
 #	Models
 #	=================
 
+import datetime
 import math
 
 from django.conf import settings
@@ -13,20 +14,36 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+# Stupidest function ever
+# This exact code directly in the default attribute throws an exception during makemigrations:
+#	TypeError: can't compare datetime.datetime to datetime.time
+# But if I move it to a function?  Works fine.
+def default_closing_time():
+	return datetime.time(hour=19, tzinfo=timezone.get_current_timezone())
+
 class state(models.Model):
 	name = models.CharField(max_length=128)
 	abbr = models.CharField(max_length=2, unique=True)
 	split_presidential = models.BooleanField(default=False, blank=True)
+	polls_close = models.TimeField(default=default_closing_time)
 	
 	senate1_class = models.PositiveSmallIntegerField(choices=settings.SENATE_CLASSES, default=0, blank=True)
 	senate2_class = models.PositiveSmallIntegerField(choices=settings.SENATE_CLASSES, default=0, blank=True)
+	
+	@property
+	def polls_open(self):
+		if timezone.now().time() > self.polls_close:
+			return False
+		else:
+			return True
 	
 	def __unicode__(self):
 		return self.name
 
 class election(models.Model):
-	year = models.PositiveSmallIntegerField(unique=True)
+	year = models.PositiveSmallIntegerField(unique=True, db_index=True)
 	past = models.BooleanField(default=False, blank=True, db_index=True)
+	day = models.DateField(blank=True)
 	
 	house_cycle = models.BooleanField(default=False, blank=True, db_index=True)
 	presidential_cycle = models.BooleanField(default=False, blank=True, db_index=True)
@@ -44,6 +61,15 @@ class election(models.Model):
 	
 	def __unicode__(self):
 		return unicode(self.year)
+	
+	def save(self, *args, **kwargs):
+		if not self.day:
+			new_day = datetime.date(year=self.year, month=11, day=2)
+			while new_day.weekday() != 1:
+				new_day = new_day.replace(day=new_day.day+1)
+			self.day = new_day
+			
+		super(election, self).save(*args, **kwargs)
 
 class election_seats(models.Model):
 	state = models.ForeignKey(state, on_delete=models.CASCADE, related_name='election_seats')
