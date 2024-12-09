8ae2e298c1cdf59340c6b9060e7d16a4e9c7f46f
Natasha L <webmaster@lupinia.net>
2024-12-08 23:57:05 -0500

    DeerConnect: Add method for merging spam_word objects (#146)

--- deerconnect/models.py
+++ deerconnect/models.py
@@ -104,6 +104,10 @@ class contact_link(link_base, access_control):
 	class Meta:
 		verbose_name = 'contact link'
 
+
+#	Spam filtering
+merge_note_delimiter = '\n=======\n'
+
 #	Keywords to scan for in contact form submissions
 class spam_word(models.Model):
 	word = models.CharField(max_length=512, unique=True)
@@ -123,6 +127,107 @@ class spam_word(models.Model):
 	def update_used_count(self):
 		"""Recalculate the cached value for used_count based on number of spam_senders referencing this word."""
 		spam_word.objects.filter(pk=self.pk).update(used_count=self.used_by.all().count())
+	
+	def split_notes(self):
+		"""
+		Retrieve the main notes body and merge history separately.
+		Returns two values, both strings:
+			notes_body:		The main body text of the notes field, if any
+			merge_history:	A newline-separated block of merge actions, if any
+		"""
+		notes_body = ''
+		merge_history = ''
+		if self.merged and 'spam_word.merge():' in self.notes:
+			if merge_note_delimiter in self.notes:
+				# There's actually something to split!
+				noteparts = self.notes.rsplit(merge_note_delimiter, 2)
+				if 'spam_word.merge():' in noteparts[1]:
+					notes_body, merge_history = noteparts
+				else:
+					# Nevermind, apparently it's a false alarm
+					notes_body = self.notes
+			
+			elif 'spam_word.merge():' in self.notes:
+				# No previous notes, just the merge history
+				merge_history = self.notes
+			
+		elif self.notes:
+			notes_body = self.notes
+		
+		return (notes_body, merge_history)
+	
+	def merge(self, target):
+		"""
+		spam_word.merge(target) -> (success, reason)
+		Merge target spam_word into this spam_word, keeping all relationships for both.
+		Target object's case_sensitive setting will be discarded
+		Notes fields will be merged, and an additional note regarding the merge added.
+		Active will be set to True if either word is set active. 
+		Oldest timestamp_post value will be kept.
+		Only this spam_word's value for word will be kept, intended use is to simplify overlaps.
+			Example: Merge 'a better solution to manage SEO' with 'manage SEO'
+		NOTE:  Object provided as target parameter will be deleted!
+		
+		Returns a tuple:
+			success: Boolean, indicates whether the merge succeeded
+			reason:  String, provides a reason for failure if success is False
+		"""
+		success = False
+		reason = ''
+		if type(self) != type(target):
+			success = False
+			reason = 'object type mismatch'
+		else:
+			target_older = False
+			if target.timestamp_post < self.timestamp_post:
+				# Use the oldest timestamp
+				self.timestamp_post = target.timestamp_post
+				target_older = True
+			
+			# Preparing to add notes indicating our actions here
+			cur_time = timezone.now().strftime('%x %X')
+			merge_history = ''
+			my_notes, my_history = self.split_notes()
+			target_notes, target_history = target.split_notes()
+			
+			if my_notes and target_notes:
+				if target_older:
+					my_notes = '%s\n---\n%s\n' % (target_notes, my_notes)
+				else:
+					my_notes = '%s\n---\n%s\n' % (my_notes, target_notes)
+			elif target_notes:
+				my_notes = target_notes
+			
+			if my_notes:
+				my_notes = '%s%s' % (my_notes, merge_note_delimiter)
+			
+			if my_history and target_history:
+				if target_older:
+					my_history = '%s\n%s' % (target_history, my_history)
+				else:
+					my_history = '%s\n%s' % (my_history, target_history)
+			elif target_history:
+				my_history = target_history
+			
+			if my_history:
+				my_history = '%s\n' % my_history
+			
+			self.notes = '%s%sspam_word.merge(): Merged %d (%s): %s' % (my_notes, my_history, target.pk, target.word, cur_time)
+			
+			# Moving affiliated spam senders
+			# I don't love that the only way to do this is a loop
+			target_senders = target.used_by.all()
+			for sender in target_senders:
+				sender.word_used.add(self)
+			
+			# Looks like we're all set!  Let's do this!
+			self.merged = True
+			self.save()
+			target.delete()
+			self.update_used_count()
+			success = True
+		
+		return (success, reason)
 	
 	class Meta:
 		verbose_name = 'spam keyword'
