47b9dbf0142d9e986fcc566af2ce038af7e50e02
Natasha L <webmaster@lupinia.net>
2024-12-26 22:38:44 -0500

    Add docstrings to awi.context_processors (#142)

--- awi/context_processors.py
+++ awi/context_processors.py
@@ -24,8 +24,15 @@ def site(request):
 	}
 
 def settings_vars(request):
-	# Add an option to switch the background to all-white, for development purposes.
-	# I can do this pretty easily using my browser's dev tools, but it'd be nice to have it built-in.
+	"""
+	Context processor to add various system settings as context variables.
+	
+	Values:
+		debug_check:  Bool, value of settings.DEBUG
+		debug_white_bg:  Bool, True if settings.DEBUG is True and ?nobg=1 in URL
+		mapbox_token:  Value of settings.MAPBOX_KEY
+		locale:  Value of settings.LANGUAGE_CODE
+	"""
 	bg_white = False
 	if settings.DEBUG and request.GET.get('nobg',False):
 		bg_white = True
@@ -38,6 +45,20 @@ def settings_vars(request):
 	}
 
 def meta(request):
+	"""
+	Context processor to set up the initial values of header meta tags. 
+	These will usually be overridden by views, but sensible defaults help a lot. 
+	
+	Values:
+		title_site:  Overall title for the whole site
+		title_page:  Page-specific title, defaults to an empty string
+		title_img:  Image filename component, just set to 'fr'
+		sitemeta_author_name:  Default content author name
+		sitemeta_twitter:  Default content author Twitter handle
+		sitemeta_page_type:  Default OpenGraph content type
+		sitemeta_is_image:  Boolean, set to True if this is primarily image content 
+		sitemeta_desc:  Default page summary
+	"""
 	return {
 		'title_site':settings.SITE_TITLE,
 		'title_page':'',
