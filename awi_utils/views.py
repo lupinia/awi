d2179f3b43ba4b72eadf20d21ba678a41b8b2c2d
Natasha L <webmaster@lupinia.net>
2024-12-27 22:36:30 -0500

    Remove deprecated SL plate generator code from Awi Utils (#71), now that #192 is implemented. This also finishes removing the random sequence functions from awi_utils.utils.

--- awi_utils/views.py
+++ awi_utils/views.py
@@ -16,9 +16,6 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.utils import timezone
 
-from awi_utils.utils import rand_license_plate
-from awi_utils.sl_plates import plate_types
-
 def json_response(request, data=''):
 	return HttpResponse(simplejson.dumps(data), content_type='application/json')
 
@@ -27,39 +24,6 @@ class placeholder(TemplateView):
 		context=super(placeholder,self).get_context_data(**kwargs)
 		return context
 
-#	This is a tangentially-related convenience tool that I didn't have a better home for
-class plate_generator(TemplateView):
-	template_name = 'sl_plategen.html'
-	
-	def get_context_data(self, **kwargs):
-		context=super(plate_generator,self).get_context_data(**kwargs)
-		context['title_page'] = "License Plate Generator"
-		
-		context['generated'] = False
-		
-		slug = kwargs.get('slug', '').lower()
-		cur_plate = plate_types.get(slug.upper(), {})
-		cur_plate_sequence = cur_plate.get('sequence', '')
-		
-		if cur_plate_sequence:
-			context['slug'] = slug
-			context['notes'] = cur_plate.get('notes', '')
-			platevalue = rand_license_plate(cur_plate_sequence)
-			if platevalue:
-				context['generated'] = True
-				context['platevalue'] = platevalue
-		
-		else:
-			plate_codes = plate_types.keys()
-			plate_codes.sort()
-			context['plate_types'] = []
-			for code in plate_codes:
-				p = plate_types[code]
-				p['code'] = code.upper()
-				p['slug'] = code.lower()
-				context['plate_types'].append(p)
-		
-		return context
 
 #	A homepage for new tabs, to just show a photo background, plus some helpful extra data
 class newtab_view(TemplateView):
