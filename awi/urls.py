1a2440d3aa1f77815e87e49595e9e7ed5eb7a61b
Natasha L <webmaster@lupinia.net>
2025-01-09 05:09:24 -0500

    Cleanup and reorganization of core URL map (awi.urls) (#213)

--- awi/urls.py
+++ awi/urls.py
@@ -34,68 +34,111 @@ handler500 = system_error
 handler403 = access_views.denied_error
 
 urlpatterns = [
+	# DeerFind: Shortcode redirect
 	url(r'^s/(?P<type>.)(?P<pk>[0-9]+)', shortcode_redirect, name='shortcode'),
 	
+	# Django sitemap view
+	url(r'^sitemap\.xml$', cache_control(max_age=60*60*48)(sitemap), {'sitemaps': SITEMAP_OBJECTS}, name='django.contrib.sitemaps.views.sitemap'),
+	
+	# Django admin views: Block without x509 cert
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin_tools/', include('admin_tools.urls')),
 	
-	url(r'^accounts/login/', login_view, name='login'),
-	url(r'^accounts/logout/', logout_view, {'template_name':'registration/login.html'}, name='logout'),
-	url(r'^accounts/age_form/$', never_cache(check_honeypot(field_name=settings.HONEYPOT_FIELD_NAME_AWIACCESS)(access_views.age_verify_full.as_view())), name='age_form'),
-	url(r'^accounts/age_form_embed/$', never_cache(check_honeypot(field_name=settings.HONEYPOT_FIELD_NAME_AWIACCESS)(access_views.age_verify.as_view())), name='age_form_embed'),
-	
+	# User authentication and management/settings
 	url(r'^settings/$', access_views.settings_page.as_view(), name='settings'),
+	url(r'^accounts/', include([
+		url(r'^login/$', login_view, name='login'),
+		url(r'^logout/$', logout_view, {'template_name':'registration/login.html'}, name='logout'),
+		url(r'^age_form/$', never_cache(check_honeypot(field_name=settings.HONEYPOT_FIELD_NAME_AWIACCESS)(access_views.age_verify_full.as_view())), name='age_form'),
+		url(r'^age_form_embed/$', never_cache(check_honeypot(field_name=settings.HONEYPOT_FIELD_NAME_AWIACCESS)(access_views.age_verify.as_view())), name='age_form_embed'),
+	])),
 	
+	# DeerFind: Main search view
 	url(r'^search/', search_view.as_view(), name='haystack_search'),
 	
-	# APIs and utility views
-	url(r'^tools/category_list/$', permission_required('deertrees.change_leaf')(deertrees_views.all_cats.as_view()), name='all_cats'),
-	url(r'^tools/sunset/(?P<slug>.*)\.json', sunset_views.geojson_image, name='sunset_geojson'),
-	url(r'^tools/backgrounds/$', sunset_views.bgtag_list.as_view(), name='sunset_bgtags_all'),
-	url(r'^tools/backgrounds/(?P<slug>.*)\.cfm', sunset_views.img_bgtag_view.as_view(), name='sunset_bgtag'),
-	url(r'^tools/plate_generator/', include('deerbuild.urls', namespace='deerbuild')),
-	url(r'^tools/newtab\.html$', never_cache(newtab_view.as_view()), name='newtab_page'),
-	
-	# Special Features
+	# DeerConnect: Contact form
 	url(r'^contact/$', never_cache(check_honeypot(contact_page.as_view())), name='contact'),
+	
+	# =====================================
+	# DeerTrees: special_feature extension URLs
+	# DeerFood
 	url(r'^personal/cooking/menu/', include('deerfood.urls', namespace='deerfood'), kwargs={'special_feature_slug':'menu'}),
+	
+	# DeerAttend
 	url(r'^furry/cons/', include('deerattend.urls',namespace='deerattend'), kwargs={'special_feature_slug':'cons'}),
 	
-	url(r'^sitemap\.xml$', sitemap, {'sitemaps': SITEMAP_OBJECTS}, name='django.contrib.sitemaps.views.sitemap'),
+	# DeerTrees sitemap view
+	url(r'^about/sitemap\.htm$', cache_control(max_age=60*60*48)(deertrees_views.sitemap.as_view()), name='sitemap_htm', kwargs={'special_feature_slug':'sitemap.htm'}),
 	
-	# DeerTrees and DeerBooks are special cases for this site.
+	# =====================================
+	# /tools/ - Special subdirectory for utility views
+	url(r'^tools/', include([
+		# DeerTrees: Full category list
+		url(r'^category_list\.ashx$', permission_required('deertrees.change_leaf')(deertrees_views.all_cats.as_view()), name='all_cats'),
+		
+		# Sunset: Background image collections (bgtags)
+		url(r'^backgrounds/$', sunset_views.bgtag_list.as_view(), name='sunset_bgtags_all'),
+		url(r'^backgrounds/(?P<slug>.*)\.cfm', sunset_views.img_bgtag_view.as_view(), name='sunset_bgtag'),
+		
+		# Sunset: GeoJSON API (deprecated)
+		url(r'^sunset/(?P<slug>.*)\.json', sunset_views.geojson_image, name='sunset_geojson'),
+		
+		# DeerBuild: Plate generator
+		url(r'^plate_generator/', include('deerbuild.urls', namespace='deerbuild')),
+		
+		# DeerSky: Newtab page
+		url(r'^newtab\.html$', never_cache(newtab_view.as_view()), name='newtab_page'),
+	])),
+	
+	# =====================================
+	# /tags/ - Special subdirectory for DeerTrees tag views
+	url(r'^tags/$', deertrees_views.all_tags.as_view(), name='all_tags'),
+	url(r'^tags/(?P<slug>.*)/', include([
+		url(r'^$', deertrees_views.tag_list.as_view(), name='tag'),
+		url(r'^feed\.rss$', cache_control(max_age=60*60*6)(deertrees_views.tag_rssfeed()), name='tag_rss'),
+		
+		# Sunset: Aggregate gallery views for tags
+		url(r'^featured\-images\.cfm$', sunset_views.img_tag_view.as_view(), name='tag_images_featured', kwargs={'viewtype':'featured'}),
+		url(r'^recent\-images\.cfm$', sunset_views.img_tag_view.as_view(), name='tag_images_recent', kwargs={'viewtype':'recent'}),
+		
+		# Sunset: Image RSS feeds
+		url(r'^featured\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_tag_feed()), name='tag_rss_images_featured', kwargs={'viewtype':'featured'}),
+		url(r'^recent\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_tag_feed()), name='tag_rss_images_recent', kwargs={'viewtype':'recent'}),
+	])),
+	
+	# =====================================
+	# DEERTREES FILESYSTEM EMULATION
+	# Homepage/root views
 	url(r'^$', deertrees_views.homepage.as_view(), name='home'),
 	url(r'^feed\.rss$', cache_control(max_age=60*60*12)(deertrees_views.main_rssfeed()), name='home_rss'),
 	
-	url(r'^tags/$', deertrees_views.all_tags.as_view(), name='all_tags'),
-	url(r'^tags/(?P<slug>.*)/$', deertrees_views.tag_list.as_view(), name='tag'),
-	url(r'^tags/(?P<slug>.*)/featured\-images\.cfm$', sunset_views.img_tag_view.as_view(), name='tag_images_featured', kwargs={'viewtype':'featured'}),
-	url(r'^tags/(?P<slug>.*)/recent\-images\.cfm$', sunset_views.img_tag_view.as_view(), name='tag_images_recent', kwargs={'viewtype':'recent'}),
-	url(r'^tags/(?P<slug>.*)/feed\.rss$', cache_control(max_age=60*60*6)(deertrees_views.tag_rssfeed()), name='tag_rss'),
-	url(r'^tags/(?P<slug>.*)/featured\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_tag_feed()), name='tag_rss_images_featured', kwargs={'viewtype':'featured'}),
-	url(r'^tags/(?P<slug>.*)/recent\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_tag_feed()), name='tag_rss_images_recent', kwargs={'viewtype':'recent'}),
-	
-	url(r'^about/sitemap\.htm$', cache_control(max_age=60*60*48)(deertrees_views.sitemap.as_view()), name='sitemap_htm', kwargs={'special_feature_slug':'sitemap.htm'}),
-	
-	url(r'^(?P<cached_url>[\w\d_/-]+)/book\.(?P<slug>.*)\.tex', deerbooks_views.book_tex.as_view(), name='book_tex'),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/book\.(?P<slug>.*)\.md', deerbooks_views.book_md.as_view(), name='book_md'),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/book\.(?P<slug>.*)\.txt', deerbooks_views.book_txt.as_view(), name='book_txt'),
-	
-	url(r'^(?P<cached_url>[\w\d_/-]+)/(?P<slug>.*)\.htm', deerbooks_views.single_page_htm.as_view(), name='page_htm'),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/(?P<slug>.*)\.txt', deerbooks_views.single_page_txt.as_view(), name='page_txt'),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/(?P<slug>.*)\.md', deerbooks_views.single_page_md.as_view(), name='page_md'),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/(?P<slug>.*)\.tex', deerbooks_views.single_page_tex.as_view(), name='page_tex'),
-	
-	url(r'^(?P<cached_url>[\w\d_/-]+)/(?P<slug>.*)\.ihtm', sunset_views.single_image.as_view(), name='image_single'),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/featured\-images\.cfm$', sunset_views.img_cat_view.as_view(), name='category_images_featured', kwargs={'viewtype':'featured'}),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/recent\-images\.cfm$', sunset_views.img_cat_view.as_view(), name='category_images_recent', kwargs={'viewtype':'recent'}),
-	
-	url(r'^(?P<cached_url>[\w\d_/-]+)/featured\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_cat_feed()), name='category_rss_images_featured', kwargs={'viewtype':'featured'}),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/recent\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_cat_feed()), name='category_rss_images_recent', kwargs={'viewtype':'recent'}),
-	url(r'^(?P<cached_url>[\w\d_/-]+)/feed\.rss$', cache_control(max_age=60*60*6)(deertrees_views.cat_rssfeed()), name='category_rss'),
-	
-	url(r'^(?P<cached_url>[\w\d_/-]+)/$', deertrees_views.category_list.as_view(), name='category'),
+	# Directories and core structure
+	url(r'^(?P<cached_url>[\w\d_/-]+)/', include([
+		# DeerBooks: Book views
+		url(r'^book\.(?P<slug>.*)\.tex', deerbooks_views.book_tex.as_view(), name='book_tex'),
+		url(r'^book\.(?P<slug>.*)\.md', deerbooks_views.book_md.as_view(), name='book_md'),
+		url(r'^book\.(?P<slug>.*)\.txt', deerbooks_views.book_txt.as_view(), name='book_txt'),
+		
+		# DeerBooks: Page views
+		url(r'^(?P<slug>.*)\.htm', deerbooks_views.single_page_htm.as_view(), name='page_htm'),
+		url(r'^(?P<slug>.*)\.txt', deerbooks_views.single_page_txt.as_view(), name='page_txt'),
+		url(r'^(?P<slug>.*)\.md', deerbooks_views.single_page_md.as_view(), name='page_md'),
+		url(r'^(?P<slug>.*)\.tex', deerbooks_views.single_page_tex.as_view(), name='page_tex'),
+		
+		# Sunset: Image views
+		url(r'^(?P<slug>.*)\.ihtm', sunset_views.single_image.as_view(), name='image_single'),
+		
+		# Aggregate gallery views and feeds for directory (Sunset)
+		url(r'^featured\-images\.cfm$', sunset_views.img_cat_view.as_view(), name='category_images_featured', kwargs={'viewtype':'featured'}),
+		url(r'^recent\-images\.cfm$', sunset_views.img_cat_view.as_view(), name='category_images_recent', kwargs={'viewtype':'recent'}),
+		url(r'^featured\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_cat_feed()), name='category_rss_images_featured', kwargs={'viewtype':'featured'}),
+		url(r'^recent\-images\.rss$', cache_control(max_age=60*60*6)(sunset_views.img_cat_feed()), name='category_rss_images_recent', kwargs={'viewtype':'recent'}),
+		
+		# Directory root (DeerTrees)
+		url(r'^feed\.rss$', cache_control(max_age=60*60*6)(deertrees_views.cat_rssfeed()), name='category_rss'),
+		url(r'^$', deertrees_views.category_list.as_view(), name='category'),
+	])),
 ]
 
 #	django-debug-toolbar
