e4849f7bee71c643e706c24e9a4065832d8fce0c
Natasha L <webmaster@lupinia.net>
2025-08-02 18:04:25 -0400

    DeerConnect: Header social icons are now cached for 24 hours, and only public icons are used in the header (#232)

--- deerconnect/context_processors.py
+++ deerconnect/context_processors.py
@@ -6,11 +6,18 @@
 #	Context Processors
 #	=================
 
+from django.conf import settings
+from django.core.cache import cache
+
 from deerconnect.models import contact_link
 from awi_access.models import access_query
 
 def social_icons(request):
-	social_links = contact_link.objects.filter(featured=True).filter(access_query(request)).order_by('label')
+	social_links = cache.get('social_links_%d' % settings.SITE_ID)
+	if social_links is None:
+		social_links = contact_link.objects.filter(featured=True).filter(access_query()).order_by('label')
+		cache.set('social_links_%d' % settings.SITE_ID, social_links, 60*60*24)
+	
 	if social_links:
 		return {'header_icons':social_links}
 	else:
