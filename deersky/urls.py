9bccd19e4ed860e398163098bd53976b019a473b
Natasha L <webmaster@lupinia.net>
2026-04-30 17:14:28 -0400

    DeerSky: Create view and placeholder template for homepages (#234)

--- deersky/urls.py
+++ deersky/urls.py
@@ -9,9 +9,9 @@
 from django.conf.urls import url
 from django.views.decorators.cache import never_cache
 
-from deersky.views import homepage_view
+from deersky.views import homepage_view, homepage_list
 
 urlpatterns = [
-	url(r'^$', never_cache(homepage_view.as_view()), name='newtab_default', kwargs={'slug':'default',}),
+	url(r'^$', homepage_list.as_view(), name='newtab_list'),
 	url(r'^(?P<slug>.*)\.jsp$', never_cache(homepage_view.as_view()), name='newtab'),
 ]
--- deersky/views.py
+++ deersky/views.py
@@ -6,7 +6,9 @@
 #	Views
 #	=================
 
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
+from django.core.urlresolvers import reverse
+from django.db.models import Q
 
 from deersky.models import homepage
 
@@ -23,5 +25,39 @@ class homepage_view(DetailView):
 		context['title_page'] = context['homepage'].title
 		context['time_local'] = context['homepage'].city.now()
 		context['time_list'] = context['homepage'].secondary_clocks
+		
+		return context
+
+class homepage_list(ListView):
+	model = homepage
+	template_name = 'deersky/newtab_list.html'
+	
+	def get_queryset(self):
+		queryset = super(homepage_list, self).get_queryset()
+		ordering = ['main_city__label',]
+		if self.request.user.is_authenticated():
+			if not self.request.user.is_superuser and not self.request.user.is_staff:
+				# Regular user
+				queryset = queryset.filter(Q(public=True) | Q(owner=self.request.user))
+				ordering.insert(0, 'public') # Put all private items at the top as a convenience
+		else:
+			queryset = queryset.filter(public=True)
+		
+		return queryset.select_related('main_city', 'owner').prefetch_related('secondary_clock_set', 'backgrounds_override').order_by(*ordering)
+	
+	def get_context_data(self, **kwargs):
+		context = super(homepage_list, self).get_context_data(**kwargs)
+		
+		context['title_view'] = 'Homepages'
+		
+		# Breadcrumbs
+		if not context.get('breadcrumbs',False):
+			context['breadcrumbs'] = []
+		
+		context['breadcrumbs'].append({'url':reverse('deersky:newtab_list'), 'title':'Homepages'})
+		
+		# Metadata
+		context['title_page'] = context['title_view']
+		context['sitemeta_desc'] = "Basic photo-background homepages with clocks and timer widgets, no ads or upsells, just a simple utility service."
 		
 		return context
