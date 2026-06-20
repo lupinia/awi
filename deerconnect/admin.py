4d2a6e0a16af3aaf4805ff2f48623a73c269d770
Natasha L <webmaster@lupinia.net>
2026-06-20 19:56:11 -0400

    DeerConnect: Add image field rendering to link and contact_link admin views (#283)

--- deerconnect/admin.py
+++ deerconnect/admin.py
@@ -9,8 +9,9 @@
 from django.contrib import admin
 from django.core.urlresolvers import reverse
 
-from deertrees.admin import leaf_admin
+from awi.utils.admin import imgfield_as_html
 from awi_access.admin import access_admin
+from deertrees.admin import leaf_admin
 
 from deerconnect.models import link, contact_link, spam_domain, spam_sender, spam_word
 
@@ -18,12 +19,21 @@ class link_admin(leaf_admin):
 	list_select_related = True
 	search_fields = ['label','url','desc'] + leaf_admin.search_fields
 	fieldsets = [
-		(None,{'fields':(('label','involved'),'url','desc','icon','icon_large'),},),
+		(None,{'fields':(('label','involved'),'url','desc',),},),
+		('Images',{'fields':(('icon','icon_admin_tag',),('icon_large','icon_large_admin_tag','icon_large_type',),)},),
 		('Health Check Options',{'fields':(('health_check','healthy'),),},),
 	] + leaf_admin.fieldsets
 	list_filter = leaf_admin.list_filter + ['involved', 'healthy']
 	list_display = ('label','url','involved','healthy',) + leaf_admin.list_display
-	readonly_fields = ['healthy',] + leaf_admin.readonly_fields
+	readonly_fields = ['healthy','icon_admin_tag','icon_large_admin_tag','icon_large_type',] + leaf_admin.readonly_fields
+	
+	def icon_admin_tag(self, obj):
+		return imgfield_as_html(obj, imgfield='icon', width='icon_width', height='icon_height', darkbg=True)
+	icon_admin_tag.short_description = 'current'
+	
+	def icon_large_admin_tag(self, obj):
+		return imgfield_as_html(obj, imgfield='icon_large', width='icon_large_width', height='icon_large_height', darkbg=True)
+	icon_large_admin_tag.short_description = 'current'
 	
 	def view_on_site(self, obj):
 		return obj.url
@@ -33,15 +43,28 @@ class link_admin(leaf_admin):
 		super(link_admin, self).save_model(request, obj, form, change)
 
 class contact_admin(access_admin):
-	list_select_related = True
+	list_select_related = ('cat',)
 	search_fields = ['label','name','url','desc']
 	fieldsets = [
-		(None,{'fields':(('label','name','im'),'url','desc','icon','icon_large','cat'),},),
+		(None,{'fields':(('label','name','im'),'url','desc','cat'),},),
+		('Images',{'fields':(('icon','icon_admin_tag',),('icon_large','icon_large_admin_tag','icon_large_type',),)},),
 		('Time Options',{'fields':(('timestamp_post','timestamp_mod',),),},),
 	] + access_admin.fieldsets
 	list_filter = access_admin.list_filter + ['im',]
-	list_display = ('label','name','url','cat','im','timestamp_post','timestamp_mod',) + access_admin.list_display
-	readonly_fields = ['timestamp_mod',]
+	list_display = ('label','icon_admin_tag_list','name','url','cat','im','timestamp_post','timestamp_mod',) + access_admin.list_display
+	readonly_fields = ['timestamp_mod','icon_admin_tag','icon_large_admin_tag','icon_large_type',]
+	
+	def icon_admin_tag(self, obj):
+		return imgfield_as_html(obj, imgfield='icon', width='icon_width', height='icon_height', darkbg=True)
+	icon_admin_tag.short_description = 'current'
+	
+	def icon_admin_tag_list(self, obj):
+		return imgfield_as_html(obj, imgfield='icon', width='icon_width', height='icon_height', default='icon_url')
+	icon_admin_tag_list.short_description = 'icon'
+	
+	def icon_large_admin_tag(self, obj):
+		return imgfield_as_html(obj, imgfield='icon_large', width='icon_large_width', height='icon_large_height', darkbg=True)
+	icon_large_admin_tag.short_description = 'current'
 	
 	def view_on_site(self, obj):
 		return obj.url
