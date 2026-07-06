ba727966c2f55a4a3d16a0894876df3c7ea34f41
Natasha L <webmaster@lupinia.net>
2026-07-06 00:03:18 -0400

    Dagasi: Add utility function for building default user preferences for unauthenticated users (#281, #303)

---
+++ dagasi/utils/users.py
@@ -0,0 +1,17 @@
+#	Dagasi - Content Security (Django App)
+#	By Natasha L.
+#	www.lupinia.net | github.com/lupinia
+#	
+#	=================
+#	Utility Functions
+#	=================
+
+from dagasi.types import status
+
+def default_userprefs():
+	"""Default user preferences"""
+	prefs = {
+		'show_mature': status(False, 'access_mature_prompt'),
+		'show_hidden': False,
+	}
+	return prefs
