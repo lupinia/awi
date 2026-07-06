055cac9e369c2be1947ed6743df31d1b34dd4a7b
Natasha L <webmaster@lupinia.net>
2026-07-05 21:56:32 -0400

    Dagasi: Restructure models into submodules (#281)

---
+++ dagasi/models/__init__.py
@@ -0,0 +1,9 @@
+#	Dagasi - Content Security (Django App)
+#	By Natasha L.
+#	www.lupinia.net | github.com/lupinia
+#	
+#	=================
+#	Models
+#	=================
+
+from .content import access_code, user_settings, blocked_ip
