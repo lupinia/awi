fef0cfe8287d7bcde8ae967be2e028ed019440d2
Natasha L <webmaster@lupinia.net>
2026-07-05 22:33:07 -0400

    Dagasi: Add related_name and on_delete to user_settings model, and use abstract relationship definition (fix #302)

--- dagasi/models/users.py
+++ dagasi/models/users.py
@@ -25,7 +25,7 @@ from dagasi.types import status
 @python_2_unicode_compatible
 class user_settings(models.Model):
 	# Core fields
-	user = models.OneToOneField(User)
+	user = models.OneToOneField('auth.User', related_name='settings', on_delete=models.CASCADE)
 	timestamp_mod = models.DateTimeField(auto_now=True, db_index=True, verbose_name='date/time modified', help_text="Timestamp showing when this item was last edited.  Automatically set with every save operation, can't be overridden.")
 	
 	# Mature content settings
