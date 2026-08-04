60a9da6cced7d14218d9c19d4e58d1924ce249c7
Natasha L <webmaster@lupinia.net>
2026-08-04 13:47:06 -0400

    Dagasi: Make it harder to block myself during testing (#382)

--- dagasi/utils/ip.py
+++ dagasi/utils/ip.py
@@ -6,15 +6,21 @@
 #	Utility Functions/Objects
 #	=================
 
+from django.conf import settings
 from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 
 from awi_access.models import blocked_ip
+from watchdeer.utils import notify
 
 #	Checks a given address, and returns True if it's blocked
 def is_blocked(address, raise_403=False):
 	status = True
 	if address:
+		if address in settings.INTERNAL_IPS:
+			# Never block an internal IP address
+			return False
+		
 		check = cache.get('blocked_ip_%s' % address)
 		if check is None:
 			check = blocked_ip.objects.filter(active=True, address=address).exists()
@@ -32,5 +38,9 @@ def is_blocked(address, raise_403=False):
 		return status
 
 def add_new_block(address, agent=None):
-	cache.set('blocked_ip_%s' % address, True, 60*60*24*7)
-	return blocked_ip.objects.update_or_create(address=address, defaults={'user_agent':agent, 'active':True})[0]
+	if address not in settings.INTERNAL_IPS:
+		cache.set('blocked_ip_%s' % address, True, 60*60*24*7)
+		return blocked_ip.objects.update_or_create(address=address, defaults={'user_agent':agent, 'active':True})[0]
+	else:
+		notify('Trusted IP Block Attempt', 'The trusted IP address %s performed an action that would have resulted in a block if it were not a trusted IP address.  Investigate further if this was unexpected.' % address)
+		return (None, False)
