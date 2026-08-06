f383980f7be2d5d5f160c2b1b328ba85057ca535
Natasha L <webmaster@lupinia.net>
2026-08-06 01:38:45 -0400

    Define login URL by pattern name (fixes #384)

--- awi/settings/core.py
+++ awi/settings/core.py
@@ -21,6 +21,7 @@ LANGUAGE_CODE = 'en-us'
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 CSRF_USE_SESSIONS = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 AUTH_PASSWORD_VALIDATORS = [
 	{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
